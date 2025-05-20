b95c00afd464a5d0afff83b2c5906afff17a24ac
Bhavya Bhardwaj <bbhardwaj303@gmail.com>
2025-05-21 00:29:20 +0530

    init

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig, UserConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
@@ -20,4 +20,4 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+}) as UserConfig); // Explicitly cast to UserConfig
